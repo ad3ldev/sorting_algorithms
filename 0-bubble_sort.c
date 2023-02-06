3cdd76c7f6654d430c5dcfd71030eb86640e1043
Adel alx <Adel@alx.online>
2023-02-06 15:40:39 -0800

    betty

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -15,19 +15,18 @@ void bubble_sort(int *array, size_t size)
 	n = (int) size;
 	if (array == NULL || size < 2)
 		return;
-	while(n > 1)
+	while (n > 1)
 	{
 		new_n = 0;
 		for (i = 1; i < n; i++)
 		{
-			if (array[i-1] > array[i])
+			if (array[i - 1] > array[i])
 			{
-				SWAP(array[i], array[i-1], int);
+				SWAP(array[i], array[i - 1], int);
 				print_array((const int *)array, size);
 				new_n = i;
 			}
 		}
 		n = new_n;
 	}
-	
 }
--- deck.h
+++ deck.h
@@ -1,12 +1,21 @@
 #ifndef DECK_H
 #define DECK_H
 
+/**
+ * enum kind_e - kind of symbol on card
+ *
+ * @SPADE: 0
+ * @HEART: 1
+ * @CLUB: 2
+ * @DIAMOND: 3
+ */
+
 typedef enum kind_e
 {
-    SPADE = 0,
-    HEART,
-    CLUB,
-    DIAMOND
+	SPADE = 0,
+	HEART,
+	CLUB,
+	DIAMOND
 } kind_t;
 
 /**
@@ -18,8 +27,8 @@ typedef enum kind_e
  */
 typedef struct card_s
 {
-    const char *value;
-    const kind_t kind;
+	const char *value;
+	const kind_t kind;
 } card_t;
 
 /**
@@ -31,9 +40,9 @@ typedef struct card_s
  */
 typedef struct deck_node_s
 {
-    const card_t *card;
-    struct deck_node_s *prev;
-    struct deck_node_s *next;
+	const card_t *card;
+	struct deck_node_s *prev;
+	struct deck_node_s *next;
 } deck_node_t;
 
 #endif /* DECK_H */
