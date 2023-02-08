4fac160736f3720bddc033836f1b1dfa5ddb6a17
Adel alx <Adel@alx.online>
2023-02-08 07:44:12 -0800

    betty

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -18,7 +18,7 @@ void shell_sort(int *array, size_t size)
 
 	for (gap = 1; gap < (size / 3);)
 		gap = gap * 3 + 1;
-	for (;gap >= 1; gap /=3)
+	for (; gap >= 1; gap /= 3)
 	{
 		for (i = gap; i < size; i++)
 		{
