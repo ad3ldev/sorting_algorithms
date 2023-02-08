bd1fb15221490bc5e468348e5496a4eff936e77f
AbdelRahman Adel AbdelFatttah <56457765+ad3ldev@users.noreply.github.com>
2023-02-08 17:19:48 +0200

    Update 3-quick_sort.c

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -15,17 +15,19 @@ int lomuto_partition(int *array, size_t size, int low, int high)
 	int pivot, i, j, temp;
 
 	pivot = array[high];
-	i = low;
+	i = low - 1;
 	for (j = low; j < high; j++)
 	{
 		if (array[j] <= pivot)
-		{
+		{	
+			i++;
 			temp = array[j];
 			array[j] = array[i];
 			array[i] = temp;
-			i++;
+			print_array(array, size);
 		}
 	}
+	i++;
 	temp = array[i];
 	array[i] = array[high];
 	array[high] = temp;
