375af0c4d6b6b28a3172f84b93240e035b67fc54
AbdelRahman Adel AbdelFatttah <56457765+ad3ldev@users.noreply.github.com>
2023-02-08 15:49:22 +0200

    removing macros

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,6 +1,20 @@
 #include "sort.h"
 
 /**
+ * swap_ints - Swap two integers in an array.
+ * @a: The first integer to swap.
+ * @b: The second integer to swap.
+ */
+void swap_ints(int *a, int *b)
+{
+	int tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+
+/**
  * bubble_sort - sorts an array using the bubble sort.
  * algorithm in ascending order.
  * @array: array of numbers to sort.
@@ -22,7 +36,7 @@ void bubble_sort(int *array, size_t size)
 		{
 			if (array[i - 1] > array[i])
 			{
-				SWAP(array[i], array[i - 1], int);
+				swap_int(array[i], array[i - 1]);
 				print_array((const int *)array, size);
 				new_n = i;
 			}
--- sort.h
+++ sort.h
@@ -3,7 +3,6 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include "macro_functions.h"
 #include "deck.h"
 
 /**
