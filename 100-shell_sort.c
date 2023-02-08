5bda2e789c426a09a8c4babcea90ce5e56df4ced
AbdelRahman Adel AbdelFatttah <56457765+ad3ldev@users.noreply.github.com>
2023-02-08 17:40:19 +0200

    Create 100-shell_sort.c

---
+++ 100-shell_sort.c
@@ -0,0 +1,36 @@
+#include "sort.h"
+
+/**
+ * shell_sort - Sort an array of integers in ascending
+ *              order using the shell sort algorithm.
+ * @array: An array of integers.
+ * @size: The size of the array.
+ *
+ * Description: Uses the Knuth interval sequence.
+ */
+void shell_sort(int *array, size_t size)
+{
+	int temp;
+	size_t gap, i, j;
+
+	if (array ==  NULL || size < 2)
+		return;
+
+	for (gap = 1; gap < size;)
+		gap = gap * 3 + 1;
+	for (;gap >= 1; gap /=3)
+	{
+		for (i = gap; i < size; i++)
+		{
+			j = i;
+			while (j >= gap && array[j - gap] > array[j])
+			{
+				temp = array[j];
+				array[j] = array[j - gap];
+				array[j - gap] = temp;
+				j -= gap;
+			}
+		}
+		print_array(array, size);
+	}
+}
