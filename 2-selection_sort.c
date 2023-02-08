053a7abb594e6752d338e38dd2a00e50f3e95691
AbdelRahman Adel AbdelFatttah <56457765+ad3ldev@users.noreply.github.com>
2023-02-08 16:23:51 +0200

    Update 2-selection_sort.c

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -9,7 +9,7 @@
  */
 void selection_sort(int *array, size_t size)
 {
-	int min, temp;
+	int temp;
 	size_t i, j, j_min;
 	
 	if (array == NULL || size < 2)
@@ -20,7 +20,7 @@ void selection_sort(int *array, size_t size)
 		j_min = i;
 		for (j = i + 1; j < size; j++)
 		{
-			if (array[j] < a[j_min])
+			if (array[j] < array[j_min])
 				j_min = j;
 		}
 		if (j_min != i)
